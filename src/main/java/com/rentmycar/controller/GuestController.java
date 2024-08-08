1a6cb61bacbbb706a54958a04d9dd712b72d0c3e
Avinash-83716 <avinashlohar763@gmail.com>
2024-08-08 11:19:18 +0530

    fix: resolve Bug in guest controller

--- src/main/java/com/rentmycar/controller/GuestController.java
+++ src/main/java/com/rentmycar/controller/GuestController.java
@@ -2,19 +2,12 @@ package com.rentmycar.controller;
 
 
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/guest")
-public class GuestController {
-
-
-
 import javax.validation.constraints.Max;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.rentmycar.service.GuestService;
 import io.swagger.v3.oas.annotations.Operation;
