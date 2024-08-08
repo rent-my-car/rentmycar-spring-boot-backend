bc413b4e4e1c23162b146c01e15d1134b89bb0b7
Abhishek khamkar <abhidk1440@gmail.com>
2024-08-08 18:27:36 +0530

    create api for get all hosts

--- src/main/java/com/rentmycar/service/HostService.java
+++ src/main/java/com/rentmycar/service/HostService.java
@@ -1,10 +1,13 @@
 package com.rentmycar.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.rentmycar.dto.GetAllUsersDto;
 import com.rentmycar.dto.RegisterHostRequestDto;
 import com.rentmycar.dto.RegisterHostResponseDto;
 
 public interface HostService {
 	Optional<RegisterHostResponseDto> registerHost(RegisterHostRequestDto registerHostRequestDto);
+	List<GetAllUsersDto>getAllHost();
 }
